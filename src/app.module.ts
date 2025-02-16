e5d8b7d58bf98d905460ada0693a1757ed335e9d
Milena Sol Aron <aronmilena.it@gmail.com>
2025-02-16 15:12:34 -0300

    chore: configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceOptions } from './configuration/orm.configuration';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...datasourceOptions,
+        autoLoadEntities: true,
+        extra: {
+          timezone: 'UTC',
+        },
+      }),
+      dataSourceFactory: async () => {
+        return new DataSource(datasourceOptions).initialize();
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
